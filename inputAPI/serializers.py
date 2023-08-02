983054f76325e84e7161e636bd8760a41da29637
Amir Kooshky <kooshkya@gmail.com>
2023-08-02 15:03:39 -0400

    refactor(serializers): clean up serializers

--- inputAPI/serializers.py
+++ inputAPI/serializers.py
@@ -11,72 +11,42 @@ class MessageSerializer(serializers.ModelSerializer):
     # In order to keep the serializer from throwing errors
     #  when an already existing id is supplied, this field has been overridden
     message_id = serializers.IntegerField()
-    from_user = TelegramUserSerializer(required=True)
-
-    def to_internal_value(self, data):
-        if "from" in data:
-            data["from_user"] = data.get("from")
-            data.pop("from")
-        return super().to_internal_value(data)
-
-    def to_representation(self, instance):
-        res = super().to_representation(instance)
-        if "from_user" in res:
-            res["from"] = res.get("from_user")
-            res.pop("from_user")
-        return res
-
-    def validate(self, data):
-        print(data)
-        if "from" in data:
-            user_data = data.get("from_user")
-            data.set("from_user", TelegramUser.create_or_update_user(user_data))
-        return data
 
     class Meta:
         model = Message
-        fields = ["message_id", "date", "text", "from_user"]
-
-    def update():
-        raise NotImplemented("Not implemented!")
-    
-    def create():
-        raise NotImplemented("Not implemented!")
+        fields = ["message_id", "date", "text"]
 
 
 class UpdateSerializer(serializers.ModelSerializer):
-    update_id = serializers.IntegerField()
-    message = MessageSerializer(required=False)
-    edited_message = MessageSerializer(required=False)
-
     class Meta:
         model = Update
-        fields = ["update_id", "message", "message_edited", "edited_message"]
-    
+        fields = ["update_id", "message", "message_edited"]
+
+    def calculate_message_field(self, message_edited, message_data):
+        message_serializer = MessageSerializer(data=message_data)
+        if message_serializer.is_valid(raise_exception=True):
+            id = message_serializer.validated_data.get("message_id")
+            query = Message.objects.filter(pk=id)
+            if query.exists():
+                instance = query.first()
+                if message_edited:
+                    new_serializer = MessageSerializer(instance, 
+                                                       data=message_serializer.validated_data)
+                    if new_serializer.is_valid():
+                        msg = new_serializer.save()
+                        return msg.message_id
+                    else:
+                        raise serializers.ValidationError("Error while trying to update pre-existing message using serializers.")
+                else:
+                    return instance.message_id
+            else:
+                msg = message_serializer.save()
+                return msg.message_id
+
     def to_internal_value(self, data):
-        
+        if "message" in data or "edited_message" in data:
+            message_edited = "edited_message" in data
+            data["message_edited"] = message_edited
+            message_data = data.get("message", data.get("edited_message"))
+            data["message"] = self.calculate_message_field(message_edited, message_data)
         return super().to_internal_value(data)
-
-
-    def fix_message_fields_in_validated_data(self, validated_data):
-        if "message" in validated_data:
-            validated_data["message_edited"] = False
-            message_data = validated_data.get("message")
-        if "edited_message" in validated_data:
-            validated_data["message_edited"] = True
-            message_data = validated_data.get("edited_message")
-            validated_data.pop("edited_message")
-
-        message = Message.create_or_update_message(message_data)
-        validated_data["message"] = message
-
-
-    def create(self, validated_data):
-        query_set = Update.objects.filter(pk=validated_data.get("update_id"))
-        if query_set.exists():
-            print("Update already existed, updating its info...")
-        new_update = Update(**validated_data)
-        new_update.save()
-
-        return new_update
-
